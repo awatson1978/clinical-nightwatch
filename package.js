d23c6f427898c791a8b173cf74d6ecbe211988e8
Abigail Watson <awatson1978@gmail.com>
2014-09-19 23:33:06 -0400

    updating package dependencies

--- package.js
+++ package.js
@@ -1,11 +1,22 @@
 Package.describe({
   summary: "Ultra-easy acceptance testing for your Meteor app with Selenium and Nightwatch.",
-  version: "1.1.3",
+  version: "1.1.4",
   git: "https://github.com/awatson1978/clinical-nightwatch"
+});
+
+
+Npm.depends({
+    'glob': '3.2.9',
+    'lodash': '2.4.1',
+    'rimraf': '2.2.8',
+    'xml2js': '0.4.2',
+    'meteor-stubs': '0.0.2'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1.1');
+
+  api.use(['underscore']);
 
 
   api.addFiles('globals.json');
