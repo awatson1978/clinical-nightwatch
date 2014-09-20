a0e7dbec3d993a65079f3eab5620400ef5fe4e44
Abigail Watson <awatson1978@gmail.com>
2014-09-20 02:25:24 -0400

    updated output directory

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Ultra-easy acceptance testing with Selenium.",
-  version: "1.2.7",
+  version: "1.2.8",
   git: "https://github.com/awatson1978/clinical-nightwatch"
 });
 
