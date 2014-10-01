98ac236b45c70d508d46f94e0ec0558c6fd2c3fd
Abigail Watson <awatson1978@gmail.com>
2014-10-01 00:52:34 -0400

    removed dependency on velocity:core

--- package.js
+++ package.js
@@ -1,30 +1,20 @@
 Package.describe({
   summary: "Ultra-easy acceptance testing with Selenium.",
-  version: "1.2.28",
+  version: "1.3.1",
+  name: "clinical:nightwatch",
   git: "https://github.com/awatson1978/clinical-nightwatch"
 });
 
 
-Npm.depends({
-    'glob': '3.2.9',
-    'lodash': '2.4.1',
-    'rimraf': '2.2.8',
-    'xml2js': '0.4.2',
-    'meteor-stubs': '0.0.2'
-});
-
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
 
-  api.use(['underscore']);
-  api.use(['livedata']);
-  api.use(['velocity:core@0.2.11']);
-
+  api.addFiles('methods.nightwatch.js', ['server']);
 
   api.addFiles('globals.json', ['server']);
   api.addFiles('launch_nightwatch_from_app_root.sh', ['server']);
   api.addFiles('launch_nightwatch_from_package.sh', ['server']);
-  api.addFiles('methods.nightwatch.js', ['server']);
+
   api.addFiles('nightwatch_from_app_root.json', ['server']);
   api.addFiles('nightwatch_from_app_root_with_saucelabs.json', ['server']);
   api.addFiles('nightwatch_from_package.json', ['server']);
@@ -35,8 +25,6 @@ Package.onUse(function(api) {
 
   api.addFiles('selenium/chromedriver', ['server']);
   api.addFiles('selenium/selenium-server-standalone-2.42.0.jar', ['server']);
-
-  api.addFiles('velocity.integration.js', ['server']);
 
 });
 
