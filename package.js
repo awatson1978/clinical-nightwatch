fd7781dcadf80fe4045a6fb861c4114d8d60951d
Abigail Watson <awatson1978@gmail.com>
2014-10-23 16:19:22 -0400

    debugOnly flag; bump to 1.4.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Ultra-easy acceptance testing with Selenium.",
-  version: "1.3.4",
+  version: "1.4.0",
   name: "clinical:nightwatch",
   git: "https://github.com/awatson1978/clinical-nightwatch",
   debugOnly: true
